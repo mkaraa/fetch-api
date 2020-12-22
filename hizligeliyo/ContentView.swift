87b3eb9af6c03c7a449340531bd045f815857348
mkara <metehan.kara95@gmail.com>
2020-12-22 19:22:17 +0300

    Initial Commit

---
+++ hizligeliyo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hizligeliyo
+//
+//  Created by Metehan kara on 22.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
